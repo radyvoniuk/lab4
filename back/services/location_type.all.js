eaf649b591f9dbb3d243b1831a43736742111f6b
radyvoniuk <111687729+radyvoniuk@users.noreply.github.com>
2023-05-23 18:03:54 +0300

    commit

---
+++ back/services/location_type.all.js
@@ -0,0 +1,17 @@
+﻿const LocationType = require('./../models/location_type')
+
+/**
+ * @param {Object} data
+ */
+module.exports = function () {
+  return new Promise((resolve, reject) => {
+    LocationType.find({}, function (err, locationTypes) {
+      console.log(locationTypes)
+      if (err) {
+        reject(err)
+      } else {
+        resolve(locationTypes)
+      }
+    })
+  })
+}
